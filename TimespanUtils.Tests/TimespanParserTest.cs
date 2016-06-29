39b18c27307e684e7014b794ec164fb8d554b12c
Peter Mihalik <peter.mihalik@gmail.com>
2016-06-29 11:29:04 +0200

    implemented the parser

--- TimespanUtils.Tests/TimespanParserTest.cs
+++ TimespanUtils.Tests/TimespanParserTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace TimespanUtils.Tests
 {
@@ -7,11 +8,57 @@ namespace TimespanUtils.Tests
     public class TimeSpanParserTest
     {
         [TestMethod]
-        [Ignore]
-        public void ShouldParseSingleItem()
+        public void ShouldCallSingleTermParserWithCorrectParams()
         {
-            var timeSpanParser = new TimeSpanParser();
+            var mockOfISingleTermParser = new Mock<ISingleTermParser>();
+
             
+            var timeSpanParser = new TimeSpanParser(mockOfISingleTermParser.Object);
+            timeSpanParser.Parse("1 week 6 hours 3 minutes 11 seconds 136 millis");
+            
+            mockOfISingleTermParser.Verify(m => m.Parse("1 week"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("6 hours"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("3 minutes"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("11 seconds"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("136 millis"), Times.Once);
+            mockOfISingleTermParser.ResetCalls();
+
+            timeSpanParser.Parse("1 week 6 hours 3 minutes 11 seconds 136 milliseconds");
+            
+            mockOfISingleTermParser.Verify(m => m.Parse("1 week"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("6 hours"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("3 minutes"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("11 seconds"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("136 milliseconds"), Times.Once);
+            mockOfISingleTermParser.ResetCalls();
+        }
+
+        [TestMethod]
+        public void ShouldCallSingleTermParserWithCorrectParamsInNotPureString()
+        {
+            var mockOfISingleTermParser = new Mock<ISingleTermParser>();
+
+            
+            var timeSpanParser = new TimeSpanParser(mockOfISingleTermParser.Object);
+            timeSpanParser.Parse("we still have 1 week 6 hours 3 minutes 11 seconds 136 millis to go");
+            
+            mockOfISingleTermParser.Verify(m => m.Parse("1 week"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("6 hours"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("3 minutes"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("11 seconds"), Times.Once);
+            mockOfISingleTermParser.Verify(m => m.Parse("136 millis"), Times.Once);
+            
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenStringDoesNotMatchAnything()
+        {
+            var mockOfISingleTermParser = new Mock<ISingleTermParser>();
+
+            
+            var timeSpanParser = new TimeSpanParser(mockOfISingleTermParser.Object);
+            var result = timeSpanParser.Parse("this string contains no timespan info");
+            Assert.IsNull(result);
         }
 
         [TestMethod]
--- TimespanUtils/TimespanParser.cs
+++ TimespanUtils/TimespanParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TimespanUtils
 {
@@ -20,9 +21,25 @@ namespace TimespanUtils
         public TimeSpanParser() : this(new SingleTermParser())
         { }
 
-        public TimeSpan Parse(string input)
+        public TimeSpan? Parse(string input)
         {
-            return new SingleTermParser().Parse(input);
+            var regex = new Regex(@"(\d+ (?:weeks?|days?|hours?|minutes?|seconds?|milliseconds?|millis?))");
+            var matchCollection = regex.Matches(input);
+            
+            if (matchCollection.Count > 0)
+            {
+                var result = TimeSpan.Zero;
+                
+                for (int i = 0; i < matchCollection.Count; i++)
+                {
+                    var match = matchCollection[i];
+                    var timeSpan = _singleTermParser.Parse(match.Groups[0].Value);
+                    result = result.Add(timeSpan);
+                }
+                return result;
+            }
+
+            return null;
         }
     }
 }
